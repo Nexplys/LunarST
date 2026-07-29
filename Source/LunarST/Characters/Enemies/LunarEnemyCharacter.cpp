e1e6a4eb4f2d1f33128b9d28a55fd4e117c4b324
Nexplys <nexplys.nathan@gmail.com>
2026-07-29 20:33:40 +0200

    Add GAS enemy character base

---
+++ Source/LunarST/Characters/Enemies/LunarEnemyCharacter.cpp
@@ -0,0 +1,87 @@
+﻿#include "Characters/Enemies/LunarEnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/Attributes/LunarAttributeSet.h"
+#include "GameplayEffectTypes.h"
+
+ALunarEnemyCharacter::ALunarEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent =
+		CreateDefaultSubobject<UAbilitySystemComponent>(
+			TEXT("AbilitySystemComponent")
+		);
+
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(
+		EGameplayEffectReplicationMode::Full
+	);
+
+	AttributeSet =
+		CreateDefaultSubobject<ULunarAttributeSet>(
+			TEXT("AttributeSet")
+		);
+}
+
+void ALunarEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AbilitySystemComponent->InitAbilityActorInfo(
+		this,
+		this
+	);
+
+	AbilitySystemComponent
+		->GetGameplayAttributeValueChangeDelegate(
+			ULunarAttributeSet::GetHealthAttribute()
+		)
+		.AddUObject(
+			this,
+			&ALunarEnemyCharacter::HandleHealthChanged
+		);
+
+	OnHealthChanged(
+		AttributeSet->GetHealth(),
+		AttributeSet->GetHealth(),
+		AttributeSet->GetMaxHealth()
+	);
+}
+
+UAbilitySystemComponent*
+ALunarEnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+const ULunarAttributeSet*
+ALunarEnemyCharacter::GetAttributeSet() const
+{
+	return AttributeSet;
+}
+
+bool ALunarEnemyCharacter::IsDead() const
+{
+	return bIsDead;
+}
+
+void ALunarEnemyCharacter::HandleHealthChanged(
+	const FOnAttributeChangeData& Data
+)
+{
+	OnHealthChanged(
+		Data.OldValue,
+		Data.NewValue,
+		AttributeSet->GetMaxHealth()
+	);
+
+	if (!bIsDead && Data.NewValue <= 0.0f)
+	{
+		bIsDead = true;
+
+		AbilitySystemComponent->CancelAllAbilities();
+
+		OnDeath();
+	}
+}
---
+++ Source/LunarST/Characters/Enemies/LunarEnemyCharacter.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "LunarEnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+class ULunarAttributeSet;
+struct FOnAttributeChangeData;
+
+UCLASS()
+class LUNARST_API ALunarEnemyCharacter
+	: public ACharacter,
+	  public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ALunarEnemyCharacter();
+
+	virtual UAbilitySystemComponent*
+	GetAbilitySystemComponent() const override;
+
+	UFUNCTION(BlueprintPure, Category = "Abilities")
+	const ULunarAttributeSet* GetAttributeSet() const;
+
+	UFUNCTION(BlueprintPure, Category = "Enemy")
+	bool IsDead() const;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void HandleHealthChanged(
+		const FOnAttributeChangeData& Data
+	);
+
+	UPROPERTY(
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "Abilities"
+	)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "Abilities"
+	)
+	TObjectPtr<ULunarAttributeSet> AttributeSet;
+
+	UPROPERTY(
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "Enemy"
+	)
+	bool bIsDead = false;
+
+	UFUNCTION(
+		BlueprintImplementableEvent,
+		Category = "Enemy|Health"
+	)
+	void OnHealthChanged(
+		float OldHealth,
+		float NewHealth,
+		float MaxHealth
+	);
+
+	UFUNCTION(
+		BlueprintImplementableEvent,
+		Category = "Enemy|Health"
+	)
+	void OnDeath();
+};
